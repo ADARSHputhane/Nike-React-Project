c70e04fbdf2d7eee8046c9125c26a31e1ff87572
Adarsh Puthane <adarshputhane@gmail.com>
2023-08-27 00:45:06 +0530

    Nike React Project

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, ShoeCard, PopularProductCard, ReviewCard };
